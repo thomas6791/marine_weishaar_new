758977f838e489fa79fe05ef1052eaaa46c4f324
thomas hunckler <hunckler.thomas@hotmail.fr>
2024-01-04 21:21:34 +0100

    add agences routes

---
+++ app/controllers/agences_controller.rb
@@ -0,0 +1,12 @@
+class AgencesController < ApplicationController
+  def index
+  end
+  def neudorf
+  end
+
+  def krutenau
+  end
+
+  def orangerie
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  get 'pages/home'
+  get 'pages/about'
+  get '/gestion-locative', to:  "pages#gestion_locative"
+
+  resources :agences do
+    collection do
+      get "agence-immobiliere-strasbourg-neudorf", to: "agences#neudorf", as: "neudorf"
+      get "agence-immobiliere-strasbourg-krutenau", to: "agences#krutenau", as: "krutenau"
+      get "agence-immobiliere-strasbourg-orangerie", to: "agences#orangerie", as: "orangerie"
+    end
+  end
+  root to: "pages#home"
 end
