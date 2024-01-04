721860acdd5aa628e255bf131e9ea36550ec91f4
thomas hunckler <hunckler.thomas@hotmail.fr>
2024-01-04 21:55:57 +0100

    change

---
+++ app/controllers/achats_controller.rb
@@ -0,0 +1,85 @@
+class AchatsController < ApplicationController
+  def index
+  end
+
+  def alsace
+  end
+
+  def bas_rhin
+  end
+
+  def haut_rhin
+  end
+
+  def strasbourg
+  end
+
+  def bourse
+  end
+
+  def esplanade
+  end
+
+  def krutenau
+  end
+
+  def centre_ville
+  end
+
+  def gare
+  end
+
+  def tribunal
+  end
+
+  def orangerie
+  end
+
+  def conseil_des_xv
+  end
+
+  def cronenbourg
+  end
+
+  def hautepierre
+  end
+
+  def poteries
+  end
+
+  def koenigshoffen
+  end
+
+  def montagne_verte
+  end
+
+  def elsau
+  end
+
+  def meinau
+  end
+
+  def neudorf
+  end
+
+  def musau
+  end
+
+  def port_rhin
+  end
+
+  def neuhof
+  end
+
+  def stockfeld
+  end
+
+  def ganzau
+  end
+
+  def robertsau
+  end
+
+  def wacken
+  end
+end
