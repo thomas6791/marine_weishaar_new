2cd8c1d20eab0c5e686c50a077400173bc459a32
thomas hunckler <hunckler.thomas@hotmail.fr>
2024-01-05 21:57:19 +0100

    add yml file

--- db/migrate/20240105203240_change_annonce_type_bien.rb
+++ db/migrate/20240105203240_change_annonce_type_bien.rb
@@ -1,8 +1,8 @@
 class ChangeAnnonceTypeBien < ActiveRecord::Migration[7.0]
   def change
     remove_column :annonces, :type_bien
-    add_column :annonces, :type_bien, :integer, default: 0
-    #add_column :annonces, :type_projet, :integer, default: 0
-    #add_column :annonces, :status, :integer, default: 0
+    add_column :annonces, :type_bien, string
+    add_column :annonces, :type_projet, :string
+    add_column :annonces, :status, :string
   end
 end
