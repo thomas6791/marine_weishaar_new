fa35b4cd84b96b352dbf9588f131a388bd8de17d
thomas hunckler <hunckler.thomas@hotmail.fr>
2024-01-05 21:46:35 +0100

    change

--- db/migrate/20240105203240_change_annonce_type_bien.rb
+++ db/migrate/20240105203240_change_annonce_type_bien.rb
@@ -2,5 +2,7 @@ class ChangeAnnonceTypeBien < ActiveRecord::Migration[7.0]
   def change
     remove_column :annonces, :type_bien
     add_column :annonces, :type_bien, :integer, default: 0
+    #add_column :annonces, :type_projet, :integer, default: 0
+    #add_column :annonces, :status, :integer, default: 0
   end
 end
