f22ea3b281e4963384d35e2a80e2601cadfa07e5
thomas hunckler <hunckler.thomas@hotmail.fr>
2024-01-06 14:40:33 +0100

    change

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,7 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
+  static targets = ["grey","volets"]
   connect() {
     console.log("hello from navbar")
+  }
+  mouseOver(event) {
+    //event.currentTarget.querySelector(".dropdown-nav").style.visibility = "visible";
+    //let heightList = event.currentTarget.querySelector(".dropdown-nav").offsetHeight;
+    //alert("mouse Over")
+    //this.menugreenTarget.classList.add("visible");
+    this.greyTarget.classList.add("visible")
+    this.voletsTarget.classList.add("visible")
+    
+  }
+  mouseOut(event) {
+    this.greyTarget.classList.remove("visible")
+    this.voletsTarget.classList.remove("visible")
+    //alert("mouse Out")
+    //event.currentTarget.querySelector(".dropdown-nav").style.visibility = "hidden";
+    //this.menugreenTarget.classList.remove("visible");
   }
 }
