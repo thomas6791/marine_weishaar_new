4d43305d98c14e3e4353d24b989f20c79a1cede6
thomas hunckler <hunckler.thomas@hotmail.fr>
2024-01-12 21:05:25 +0100

    change

---
+++ db/migrate/20240112195513_change_addresse_to_annonces.rb
@@ -0,0 +1,10 @@
+class ChangeAddresseToAnnonces < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :annonces, :addresse, :string
+    add_column :annonces, :address, :string
+    add_column :annonces, :quartier, :string
+    add_column :annonces, :surface, :float
+    add_column :annonces, :dpe, :string
+    add_column :annonces, :ges, :string
+  end
+end
