77218836c54574de880f4353615db41b958e9e01
thomas hunckler <hunckler.thomas@hotmail.fr>
2024-01-21 19:26:09 +0100

    change

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+
   def home
     @annonces = Annonce.all
   end
@@ -7,6 +8,11 @@ class PagesController < ApplicationController
   end
 
   def contact
+    #response = HTTParty.get('https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJ9xTd-HVmkUcRrccf_XqEHRk&fields=address_components,name&key=AIzaSyC74ObwjB-HWFHBjvCyZUpgduKw-uQQ7a4')
+    #"https://maps.googleapis.com/maps/api/place/textsearch/json?query=salon+de+beaute+in+Strasbourg&key=AIzaSyC74ObwjB-HWFHBjvCyZUpgduKw-uQQ7a4"
+    #"https://maps.googleapis.com/maps/api/place/textsearch/json?query=pharmacie+in+Strasbourg&key=AIzaSyC74ObwjB-HWFHBjvCyZUpgduKw-uQQ7a4"
+    #puts response.body, response.code, response.message, response.headers.inspect
+
   end
 
   def agences
@@ -14,4 +20,5 @@ class PagesController < ApplicationController
 
   def gestion_locative
   end
+
 end
