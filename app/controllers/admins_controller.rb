be1e8a71060ade2c108e655d8b758f32387126e1
thomas hunckler <hunckler.thomas@hotmail.fr>
2024-10-13 19:47:58 +0200

    change

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,11 +1,13 @@
 class AdminsController < ApplicationController
   before_action :authenticate_admin!
+  layout "admin"
   def index
     if current_admin.present?
       redirect_to admin_path(current_admin)
     end
   end
   def show
+    @annonces = Annonce.all
   end
   
 end
