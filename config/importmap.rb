2fb49e75cdf058bea21f81878d16c458abe660f3
thomas hunckler <hunckler.thomas@hotmail.fr>
2024-10-20 18:05:50 +0200

    change

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,8 +4,6 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.2.0/dist/chart.js"
-pin "@kurkle/color", to: "https://ga.jspm.io/npm:@kurkle/color@0.3.2/dist/color.esm.js"
 
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "mapbox-gl" # @3.0.1
