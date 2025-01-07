85ffd457ac0fb0b56404e83c16ab04d2257f0e72
thomas hunckler <hunckler.thomas@hotmail.fr>
2025-01-07 21:02:36 +0100

    change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,5 +76,4 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.action_mailer.delivery_method = :letter_opener
 end
