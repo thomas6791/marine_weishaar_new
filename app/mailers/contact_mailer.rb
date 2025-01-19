4b254e3d64dcf16086bb707f62d1adfcc59e65f8
thomas hunckler <hunckler.thomas@hotmail.fr>
2025-01-19 18:40:00 +0100

    letter opener works

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,7 @@ class ContactMailer < ApplicationMailer
     default from: "contact@marine-weishaar.com"
     
     def contact_email
+        mail(to: "to@example.com", subject:"test")
         #@user = params[:user]
         #@url  = "http://example.com/login"
         #mail(to: @user.email, subject: "Welcome to My Awesome Site")
