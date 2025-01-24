b4f58018043187bc27e6ae6a1ee6655657a608f0
thomas hunckler <hunckler.thomas@hotmail.fr>
2025-01-24 13:34:08 +0100

    change

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,12 @@ class ContactMailer < ApplicationMailer
     default from: "contact@marine-weishaar.com"
     
     def contact_email
-        mail(to: "hunckler.thomas@hotmail.fr", subject:"test")
+        @sender = params[:sender]
+        @object = params[:object]
+        @person = params[:person]
+        @tel = params[:tel]
+        @message = params[:message]
+        mail(to: "mw@marine-weishaar.com")
         #@user = params[:user]
         #@url  = "http://example.com/login"
         #mail(to: @user.email, subject: "Welcome to My Awesome Site")
