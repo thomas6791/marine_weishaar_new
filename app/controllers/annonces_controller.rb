b7e2d0c0c8f699c209203a5a0d67bca22fefdf9d
thomas hunckler <hunckler.thomas@hotmail.fr>
2026-06-26 14:26:56 +0200

    change

--- app/controllers/annonces_controller.rb
+++ app/controllers/annonces_controller.rb
@@ -39,6 +39,6 @@ class AnnoncesController < ApplicationController
   end
 
   def annonce_params
-    params.require(:annonce).permit(:titre, :city,:address,:zipcode,:latitude,:longitude,:quartier,:country,:description,:surface,:dpe,:ges,:type_bien,:price,:ref_annonce,:nb_pieces,:nb_etage,:chauffage,:chauffage_mode,:etat_general,:annee_construction,:ascenseur,:copropriete,:cuisine_equipee,:jardin,:balcon,:terrasse,:garage ,photos:[])
+    params.require(:annonce).permit(:titre, :city,:address,:zipcode,:latitude,:longitude,:quartier,:country,:description,:surface,:dpe,:ges,:type_bien,:price,:ref_annonce,:nb_pieces,:nb_etage,:nb_chambres,:chauffage,:chauffage_mode,:etat_general,:annee_construction,:ascenseur,:copropriete,:cuisine_equipee,:jardin,:balcon,:terrasse,:garage ,photos:[])
   end
 end
